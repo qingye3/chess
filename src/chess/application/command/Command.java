f125ffc05663252d9eb24dd8f6f4f34675ab53ce
qingye3 <qingye3@illinois.edu>
2015-02-26 17:54:30 -0600

    rename command to Command

---
+++ src/chess/application/command/Command.java
@@ -0,0 +1,7 @@
+package chess.application.command;
+
+/**
+ * Created by Qing on 2/26/2015.
+ */
+public class Command {
+}
--- src/chess/application/command/command.java
+++
@@ -1,7 +0,0 @@
-package chess.application.command;
-
-/**
- * Created by Qing on 2/25/2015.
- */
-public class command {
-}
