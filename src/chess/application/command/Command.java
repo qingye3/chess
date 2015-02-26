33ccd955a762f3f0a05a6ac0c59bb562dd5c9b5f
qingye3 <qingye3@illinois.edu>
2015-02-26 17:54:47 -0600

    added empty command

--- src/chess/application/command/Command.java
+++ src/chess/application/command/Command.java
@@ -1,7 +1,14 @@
 package chess.application.command;
+import chess.lib.exception.ChessException;
 
 /**
- * Created by Qing on 2/26/2015.
+ * Created by Qing on 2/25/2015.
+ * Command interface for the command pattern
+ * Constructor must save all the information needed in order to execute the command
  */
-public class Command {
+public interface Command {
+    public void execute() throws ChessException;
+    public void unexecute();
+    @Override
+    public String toString();
 }
